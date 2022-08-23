9e3e718f1f6c5978a32d02bdaa346d7480559434
Gibril1 <gibrilissakaiddrisu@gmail.com>
2022-08-23 10:22:05 -0700

    crud application for blog done

--- backend/controller/blogController.js
+++ backend/controller/blogController.js
@@ -1,9 +1,11 @@
 const asyncHandler = require('express-async-handler')
+const Blog = require('../models/blogModel')
 // @desc Get all blogs
 // @route GET /api/blogs
 // @access Public
 const getBlogs = asyncHandler(async(req, res) => {
-    res.status(200).json({ message: 'Get blogs'})
+    const blogs = await Blog.find()
+    res.status(200).json(blogs)
 }) 
 // @desc Get user blogs
 // @route GET /api/blogs
@@ -19,19 +21,39 @@ const setBlogs = asyncHandler(async(req, res) => {
         res.status(400)
         throw new Error('Please add all the fields')
     }
-    res.status(200).json({ message: 'Set blogs'})
+    const blog = await Blog.create({
+        title: req.body.title,
+        content: req.body.content
+    })
+
+    res.status(200).json(blog)
 }) 
 // @desc Update users blogs
 // @route PUT /api/blogs
 // @access Private
 const updateBlogs = asyncHandler(async(req, res) => {
-    res.status(200).json({ message: `Update Blog ${req.params.id}`})
+    const blog = await Blog.findById(req.params.id)
+    if(!blog){
+        res.status(400)
+        throw new Error('Blog not found')
+    }
+
+    const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, req.body, { new : true})
+
+    res.status(200).json(updatedBlog)
 } )
 // @desc delete users blogs
 // @route DELETE /api/blogs
 // @access Private
 const deleteBlogs = asyncHandler(async(req, res) => {
-    res.status(200).json({ message: `Delete Blog ${req.params.id}`})
+    const blog = await Blog.findById(req.params.id)
+    if(!blog){
+        res.status(400)
+        throw new Error('Blog not found')
+    }
+
+    await blog.remove()
+    res.status(200).json({ id: req.params.id })
 } )
 
 module.exports = {
--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,17 @@ const express = require('express')
 const dotenv = require('dotenv').config()
 const colors = require('colors')
 const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
 
-
+connectDB()
 const app = express()
 
 // body middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended : false}))
 
-app.use('/api/goals', require('./routes/blogRoutes'))
+app.use('/api/blogs', require('./routes/blogRoutes'))
 
 app.use(errorHandler)
 
