726ccd717069539a0033da27d9f4f8ae58121c52
Gibril1 <gibrilissakaiddrisu@gmail.com>
2022-08-23 09:43:19 -0700

    error handler

--- backend/controller/blogController.js
+++ backend/controller/blogController.js
@@ -1,33 +1,38 @@
+const asyncHandler = require('express-async-handler')
 // @desc Get all blogs
 // @route GET /api/blogs
 // @access Public
-const getBlogs = (req, res) => {
+const getBlogs = asyncHandler(async(req, res) => {
     res.status(200).json({ message: 'Get blogs'})
-} 
+}) 
 // @desc Get user blogs
 // @route GET /api/blogs
 // @access Private
-const getUserBlogs = (req, res) => {
+const getUserBlogs = asyncHandler(async(req, res) => {
     res.status(200).json({ message: 'Get User blogs'})
-} 
+}) 
 // @desc Set users blogs
 // @route POST /api/blogs
 // @access Private
-const setBlogs = (req, res) => {
+const setBlogs = asyncHandler(async(req, res) => {
+    if(!req.body.title || !req.body.content){
+        res.status(400)
+        throw new Error('Please add all the fields')
+    }
     res.status(200).json({ message: 'Set blogs'})
-} 
+}) 
 // @desc Update users blogs
 // @route PUT /api/blogs
 // @access Private
-const updateBlogs = (req, res) => {
+const updateBlogs = asyncHandler(async(req, res) => {
     res.status(200).json({ message: `Update Blog ${req.params.id}`})
-} 
+} )
 // @desc delete users blogs
 // @route DELETE /api/blogs
 // @access Private
-const deleteBlogs = (req, res) => {
+const deleteBlogs = asyncHandler(async(req, res) => {
     res.status(200).json({ message: `Delete Blog ${req.params.id}`})
-} 
+} )
 
 module.exports = {
     getBlogs,
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const colors = require('colors')
+const { errorHandler } = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000
 
 
 const app = express()
 
+// body middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended : false}))
+
 app.use('/api/goals', require('./routes/blogRoutes'))
+
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server running at port ${port}`))
